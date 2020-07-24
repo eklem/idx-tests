2c0997e6b0cb7c3d0dbc0c313775bc3027c4fe1b
Espen Klem <espen.klem@gmail.com>
2020-07-24 14:56:56 +0200

    Trying web worker

--- async-index-and-search/search-app.js
+++ async-index-and-search/search-app.js
@@ -1,22 +1,6 @@
 // "lazy load"- db may not be immediately initialized
 const db = searchIndex({ name: 'searchIndexInBrowser' })
 
-const readJSONData = function (url) {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => indexJSONData(data))
-}
-
-const indexJSONData = function (data) {
-  db.PUT(data)
-    .then(function (message) {
-      console.log(message)
-    })
-    .catch(function (err) {
-      console.log('Error while indexing:')
-      console.log(err.message)
-    })
-}
 const search = function (q) {
   emptyElements(['searchResults'])
   db.SEARCH(...(q.split(' ')))
@@ -55,8 +39,7 @@ const emptyElements = function (elements) {
   })
 }
 
-// Index some data each time page loads (should add an _id on each object in array)
-readJSONData('https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-red-and-white.json')
+const webworker = new Worker("worker-indexing.js");
 
 // Listen to key up and initiate a search
 document.getElementById("searchQuery").onkeyup = function() {
---
+++ async-index-and-search/worker-indexing.js
@@ -0,0 +1,20 @@
+console.log('Indexing')
+const readJSONData = function (url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => indexJSONData(data))
+}
+
+const indexJSONData = function (data) {
+  db.PUT(data)
+    .then(function (message) {
+      console.log(message)
+    })
+    .catch(function (err) {
+      console.log('Error while indexing:')
+      console.log(err.message)
+    })
+}
+
+// Index some data each time page loads (should add an _id on each object in array)
+readJSONData('https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-red-and-white.json')
