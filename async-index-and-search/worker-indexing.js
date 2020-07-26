643025892b94c407df8ae38b52174f8403933bfc
Espen Klem <espen.klem@gmail.com>
2020-07-26 09:29:32 +0200

    Indentingj

--- async-index-and-search/worker-indexing.js
+++ async-index-and-search/worker-indexing.js
@@ -1,25 +1,25 @@
 importScripts('https://fergiemcdowall.github.io/search-index/dist/search-index.1.0.6.js');
-// "lazy load"- db may not be immediately initialized
+
 searchIndex({ name: 'wineDB' }, (err, db) => {
   // db is guaranteed to be open and available
   console.log('Indexing')
-const readJSONData = function (url) {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => indexJSONData(data))
-}
+  const readJSONData = function (url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => indexJSONData(data))
+  }
 
-const indexJSONData = function (data) {
-  db.PUT(data)
-    .then(function (message) {
-      console.log(message)
-    })
-    .catch(function (err) {
-      console.log('Error while indexing:')
-      console.log(err.message)
-    })
-}
+  const indexJSONData = function (data) {
+    db.PUT(data)
+      .then(function (message) {
+        console.log(message)
+      })
+      .catch(function (err) {
+        console.log('Error while indexing:')
+        console.log(err.message)
+      })
+  }
 
-// Index some data each time page loads (should add an _id on each object in array)
-readJSONData('https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-red-and-white.json')
+  // Index some data each time page loads (should add an _id on each object in array)
+  readJSONData('https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-red-and-white.json')
 })
