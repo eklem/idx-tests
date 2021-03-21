275a630e1c74898db637579764fce59b5bfab619
Espen Klem <espen.klem@gmail.com>
2021-03-21 10:01:43 +0100

    testing

--- running-in-browser/search-app.js
+++ running-in-browser/search-app.js
@@ -1,23 +1,41 @@
+// let si = null
+
 window.SearchIndex({
   name: 'myIndex'
 })
 
-const index = async () => {
+const index = async (url) => {
   // initialize an index
+  // Promise.all([
+  //   window.SearchIndex({
+  //     name: 'myIndex'
+  //   }),
+  //   window.fetch(url).then(res => res.json())
+  // ]).then(([thisSi, dump]) => {
+  //   // set global variable (in practice you might not want to do this)
+  //   si = thisSi
+  //   // replicate pregenerated index
+  //   si.PUT(dump)
+  // }).catch(console.log)
+
   const { PUT } = await SearchIndex()
 
-  // add documents to the index
-  const putResult = await PUT([{
-    _id: '1',
-    text: 'the first document'
-  }, {
-    _id: '2',
-    text: 'the second document'
-  }])
-  console.log('PUT feedback' + JSON.stringify(putResult))
+  window.fetch(url)
+    .then(response => response.json())
+    .then(JSONdata => {
+      console.log(JSON.stringify(JSONdata))
+      PUT(JSONdata)
+    })
+    .then(PutResponse => {
+      console.log('Indexing response: ' + JSON.stringify(PutResponse))
+    })
+    .catch(function (err) {
+      console.log('Error while indexing: \n' + err.message)
+    })
 }
 
 document.getElementById('index').onclick = function () {
+  const url = document.getElementById('indexUrl').value
   console.log('starting indexing...')
-  index()
+  index(url)
 }
